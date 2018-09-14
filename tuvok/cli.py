b6bc8e7836acd67bbe3f531636a3af8193842f3f
johnctitus <john.titus@rackspace.com>
2018-09-14 15:07:01 -0500

    Implement detailed messages for check failures

--- tuvok/cli.py
+++ tuvok/cli.py
@@ -1,7 +1,13 @@
 from tuvok import __version__
 import argparse
-import json
 import os
+import platform
+
+
+def translate_jq(query):
+    if platform.system() == 'Windows':
+        return '\"{}\"'.format(query.replace('"', '\\\"'))
+    return "'{}'".format(query)
 
 
 def main():
@@ -10,25 +16,26 @@ def main():
     parser.add_argument('--version', '-v',
                         action='version',
                         version='version %s' % __version__)
-    # parser.add_argument('--hi', '-w', dest='hi',
-    #                     help='Hi Everybody!!!',
-    #                     required=False)
     parser.add_argument('--file', '-f', dest='files',
                         help='File to be scanned', action='append',
                         required=False)
     args = parser.parse_args()
     checks = [
-        "'.variable[] | {_variable_name: . | keys[], _data: .[]} | select(._data[].description == null) | ._variable_name'",
-        "'.variable[] | {_variable_name: . | keys[], _data: .[]} | select(._data[].type == null) | ._variable_name'"
+        {
+            'errorfmt': "Error: Variables must contain description",
+            'jq': '.variable[] | {_variable_name: . | keys[], _data: .[]} | select(._data[].description == null) | ._variable_name',
+        },
+        {
+            'errorfmt': "Error: Variables must contain type",
+            'jq': '.variable[] | {_variable_name: . | keys[], _data: .[]} | select(._data[].type == null) | ._variable_name'
+        }
     ]
     if args.files:
         for f in args.files:
-
-            print(f)
             for check in checks:
-                output = os.popen('json2hcl --reverse < {} | jq -rc {}'.format(f, check)).read()
-                # output = os_popen('echo {} | jq ')
-                print(output)
+                output = os.popen('json2hcl --reverse < {} | jq -rc {}'.format(f, translate_jq(check['jq']))).read()
+                for entry in output.split():
+                    print("[{}] {}: {}".format(f, check['errorfmt'], entry))
 
     else:
         parser.print_help()
