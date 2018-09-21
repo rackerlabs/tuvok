bfbc625668c790790f1d0f21dd3f9c82aab5695c
johnctitus <john.titus@rackspace.com>
2018-09-21 17:53:28 -0500

    Implement configuration files and config overrides

--- tests/test_variable.py
+++ tests/test_variable.py
@@ -1,6 +1,7 @@
 from tuvok import cli
 from helpers.wrap import Wrap
 
+
 class TestVariable(object):
     def setup(self):
         self.main = cli.main
@@ -10,17 +11,41 @@ class TestVariable(object):
 
     def test_fails_if_variable_has_no_type(self, capsys):
         file = 'tests/test_variable/variable_has_no_type.tf'
+
         with Wrap(self, [file]):
             err = capsys.readouterr().err
-            assert ('[' + file + '] ERROR-Variables must contain type: foo') in err
+            assert ('Variables must contain type in {}:foo'.format(file)) in err
 
     def test_fails_if_variable_has_no_description(self, capsys):
         file = 'tests/test_variable/variable_has_no_description.tf'
         with Wrap(self, [file]):
             err = capsys.readouterr().err
-            assert ('[' + file + '] ERROR-Variables must contain description: foo') in err
+            assert ('Variables must contain description in {}:foo'.format(file)) in err
+
+    def test_fails_prevent_override_severity(self, capsys):
+        file = 'tests/test_variable/variable_has_type_and_description.tf'
+        config = 'tests/test_variable/override.tuvok.json'
+        with Wrap(self, [file], [config]):
+            err = capsys.readouterr().err
+            assert ('Cannot override check V100 in Configuration file {}'.format(config)) in err
+
+    def test_fails_prevent_override_ignore(self, capsys):
+        file = 'tests/test_variable/variable_has_type_and_description.tf'
+        config = 'tests/test_variable/ignore.tuvok.json'
+        with Wrap(self, [file], [config]):
+            err = capsys.readouterr().err
+            assert ('Cannot ignore check V100 in Configuration file {}'.format(config)) in err
+
+    def test_passes_override_success(self, capsys):
+        file = 'tests/test_variable/variable_has_type_and_description.tf'
+        config = 'tests/test_variable/success.tuvok.json'
+        with Wrap(self, [file], [config], expect_exit=False):
+            out, err = capsys.readouterr()
+            assert err == ''
+            assert ('Rule O100 will be set to severity INFO by custom config {}'.format(config)) in out
+            assert ('Rule V101 will be ignored by custom config {}'.format(config)) in out
 
     def test_passes_if_variable_has_type_and_description(self, capsys):
-        with Wrap(self, ['tests/test_variable/variable_has_type_and_description.tf'], False):
+        with Wrap(self, ['tests/test_variable/variable_has_type_and_description.tf'], expect_exit=False):
             err = capsys.readouterr().err
             assert err == ''
--- tuvok/cli.py
+++ tuvok/cli.py
@@ -1,12 +1,49 @@
 from tuvok import __version__
 import argparse
+import json
 import os
 import platform
 import re
 import subprocess
 import sys
 
-EXCLUSIONS = ['\.git', '\.terraform', '\.circleci', '__pycache__', '.*\.egg-info']
+EXCLUSIONS = [r'.git', r'.terraform', r'.circleci', '__pycache__', r'*.egg-info']
+
+
+def load_config(file, merge=None):
+    if not os.path.exists(file) and merge is None:
+        raise Exception("Error:  Configuration file {} does not exist".format(file))
+    elif not os.path.exists(file):
+        print("Warning:  Custom configuration file {} does not exist".format(file))
+        return merge
+
+    with open(file) as f:
+        config = json.load(f)
+    if merge is None:
+        if 'checks' not in config:
+            raise Exception("Error:  No checks defined in Configuration file {}".format(file))
+        if 'ignore' not in config:
+            config['ignore'] = []
+        return config
+    else:
+        for (key, value) in config.get('checks', {}).items():
+            if key in merge['checks']:
+                if merge['checks'][key].get('prevent_override', False):
+                    print("Error: Cannot override check {} in Configuration file {}".format(key, file), file=sys.stderr)
+                    sys.exit('Exiting...')
+                print("Rule {} will be set to severity {} by custom config {}".format(key, value['severity'], file))
+                merge['checks'][key]['severity'] = value['severity']
+            else:
+                print("Adding new config rule {}:{}-{}".format(key, value['severity'], value['description']))
+                merge['checks'][key] = value
+        for rule in config.get('ignore', []):
+            if rule in merge['checks'] and merge['checks'][rule].get('prevent_override', False):
+                    print("Error: Cannot ignore check {} in Configuration file {}".format(rule, file), file=sys.stderr)
+                    sys.exit('Exiting...')
+            if rule not in merge['ignore']:
+                print("Rule {} will be ignored by custom config {}".format(rule, file))
+                merge['ignore'].append(rule)
+        return merge
 
 
 def translate_jq(query):
@@ -24,28 +61,16 @@ def main():
     parser.add_argument('--file', '-f', dest='files',
                         help='File to be scanned', action='append',
                         required=False)
+    parser.add_argument('--config', '-c', dest='config', default=[],
+                        help='Custom configuration files to be loaded', action='append',
+                        required=False)
     parser.add_argument('--directory', '-d', dest='directory',
                         help='Directory to be scanned', default=os.getcwd(),
                         required=False)
     args = parser.parse_args()
-    checks = {
-        'error': [
-            {
-                'errorfmt': "Variables must contain description",
-                'jq': '.variable[] | {_variable_name: . | keys[], _data: .[]} | select(._data[].description == null) | ._variable_name',
-            },
-            {
-                'errorfmt': "Variables must contain type",
-                'jq': '.variable[] | {_variable_name: . | keys[], _data: .[]} | select(._data[].type == null) | ._variable_name'
-            }
-        ],
-        'warning': [
-            {
-                'errorfmt': "Outputs should contain description",
-                'jq': '.output[] | {_output_name: . | keys[], _data: .[]} | select(._data[].description == null) | ._output_name'
-            }
-        ],
-    }
+
+    config = load_config(os.path.join(os.path.dirname(__file__), '.tuvok.json'))
+
     files = []
     if args.files:
         file_not_exist = [file for file in args.files if not os.path.exists(file)]
@@ -55,24 +80,33 @@ def main():
     else:
         if not os.path.isdir(args.directory):
             raise Exception("Error: Directory {} does not exist".format(args.directory))
+        custom_config = os.path.join(args.directory, '.tuvok.json')
+        if os.path.exists(custom_config):
+            args.config.append(custom_config)
         for root, subdir_list, file_list in os.walk(args.directory):
-            if any(re.search('[\\\\/]{}([\\\\/].*)?$'.format(x), root) is not None for x in EXCLUSIONS):
+            if any(re.search(r'[\\/]{}([\\/].*)?$'.format(x), root) is not None for x in EXCLUSIONS):
                 continue
-            files.extend([os.path.join(root, file) for file in file_list if re.search('\.tf$', file)])
+            files.extend([os.path.join(root, file) for file in file_list if re.search(r'.tf$', file)])
+
+    for c in args.config:
+        config = load_config(c, config)
 
     error_encountered = False
     for f in sorted(set(files)):
-        for check_type, check_list in checks.items():
-            for check in check_list:
-                query = 'json2hcl --reverse < {} | jq -rc {}'.format(f, translate_jq(check['jq']))
+        for check, check_details in config['checks'].items():
+            if check in config['ignore']:
+                continue
+            if check_details['type'] == 'jq':
+                query = 'json2hcl --reverse < {} | jq -rc {}'.format(f, translate_jq(check_details['jq']))
                 (stdout, stderr) = subprocess.Popen(query, shell=True, stdout=subprocess.PIPE,
                                                     stderr=subprocess.PIPE, universal_newlines=True).communicate()
                 if 'Cannot iterate over null' in stderr:
                     continue
                 for entry in stdout.split():
-                    if 'error' in check_type:
+                    if 'ERROR' in check_details['severity'].upper():
                             error_encountered = True
-                    print("[{}] {}-{}: {}".format(f, check_type.upper(), check['errorfmt'], entry), file=sys.stderr)
+                    print("[{}] {}-{} in {}:{}".format(check_details['severity'].upper(), check,
+                                                       check_details['description'], f, entry), file=sys.stderr)
 
     if error_encountered:
         sys.exit("Validation errors reported.")
