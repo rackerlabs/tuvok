fcd1b9f54a73f446fd79efe9db9022312cea877a
Martin Smith <martin@mbs3.org>
2018-10-04 11:54:06 -0400

    Also check for github.com/ short syntax

--- tests/test_module.py
+++ tests/test_module.py
@@ -9,14 +9,25 @@ class TestModule(object):
     def teardown(self):
         self.main = None
 
-    def test_warns_if_module_isnt_pinned(self, capsys):
+    def test_warns_if_module_isnt_pinned_git(self, capsys):
         file = 'tests/test_module/module_git_missingref.tf'
         with Wrap(self, [file], expect_exit=True):
             err = capsys.readouterr().err
             assert ('Modules sourced from GitHub should be pinned in {}'.format(file)) in err
 
-    def test_passes_if_module_has_ref(self, capsys):
+    def test_passes_if_module_has_ref_git(self, capsys):
         with Wrap(self, ['tests/test_module/module_git_hasref.tf'], expect_exit=False):
+            err = capsys.readouterr().err
+            assert err == ''
+
+    def test_warns_if_module_isnt_pinned_github(self, capsys):
+        file = 'tests/test_module/module_github_missingref.tf'
+        with Wrap(self, [file], expect_exit=True):
+            err = capsys.readouterr().err
+            assert ('Modules sourced from GitHub should be pinned in {}'.format(file)) in err
+
+    def test_passes_if_module_has_ref_github(self, capsys):
+        with Wrap(self, ['tests/test_module/module_github_hasref.tf'], expect_exit=False):
             err = capsys.readouterr().err
             assert err == ''
 
---
+++ tests/test_module/module_github_hasref.tf
@@ -0,0 +1,3 @@
+module "some_module" {
+  source = "github.com/test//modules//terraform?ref=v1.2.3"
+}
