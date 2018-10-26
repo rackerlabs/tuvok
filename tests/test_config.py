dcb55ec056b05c93321ea1a319656a1d72bac9ea
Martin Smith <martin@mbs3.org>
2018-10-26 13:03:14 -0400

    Don't use opaque IDs, use names
    Switch to more readable names of rules.
    
    Fixes https://github.com/rackerlabs/tuvok/issues/22

--- tests/test_config.py
+++ tests/test_config.py
@@ -13,13 +13,13 @@ class TestConfig(object):
         file = 'tests/test_config/valid.tf'
         config = 'tests/test_config/override.tuvok.json'
         with Wrap(self, [file], [config]):
-            assert ('Cannot override check TUVOK100 in Configuration file {}'.format(config)) in caplog.text
+            assert ('Cannot override check variable_description in Configuration file {}'.format(config)) in caplog.text
 
     def test_fails_prevent_override_ignore(self, caplog):
         file = 'tests/test_config/valid.tf'
         config = 'tests/test_config/ignore.tuvok.json'
         with Wrap(self, [file], [config]):
-            assert ('Cannot ignore check TUVOK100 in Configuration file {}'.format(config)) in caplog.text
+            assert ('Cannot ignore check variable_description in Configuration file {}'.format(config)) in caplog.text
 
     def test_passes_override_success(self, capsys, caplog):
         file = 'tests/test_config/valid.tf'
@@ -27,5 +27,5 @@ class TestConfig(object):
         out, err = capsys.readouterr()
         with Wrap(self, [file], [config], expect_exit=False):
             assert err == ''
-            assert ('Rule TUVOK103 will be set to severity INFO by custom config {}'.format(config)) in caplog.text
-            assert ('Rule TUVOK101 will be ignored by custom config {}'.format(config)) in caplog.text
+            assert ('Rule output_description will be set to severity INFO by custom config {}'.format(config)) in caplog.text
+            assert ('Rule variable_type will be ignored by custom config {}'.format(config)) in caplog.text
