708d6fcff46ad0f38f231f1f3d17de0c0a5c8d24
Martin Smith <martin@mbs3.org>
2018-11-28 08:44:01 -0500

    Add the [PASS] that was missing on two tests

--- tests/test_module.py
+++ tests/test_module.py
@@ -32,7 +32,7 @@ class TestModule(object):
     def test_passes_if_module_not_git(self, caplog):
         file = 'tests/test_module/module_notgit.tf'
         with Wrap(self, [file], expect_exit=False):
-            assert ('Modules sourced from GitHub should be pinned FAIL in {}'.format(file)) not in caplog.text
+            assert ('[PASS] github_module_ref:Modules sourced from GitHub should be pinned:{}'.format(file)) in caplog.text
 
     def test_fails_if_module_has_rackspace_http_ref(self, caplog):
         file = 'tests/test_module/module_github_has_rackspace_ref.tf'
@@ -42,7 +42,7 @@ class TestModule(object):
     def test_passes_if_module_has_rackspace_ssh_ref(self, caplog):
         file = 'tests/test_module/module_git_has_rackspace_ref.tf'
         with Wrap(self, [file], expect_exit=False):
-            assert ('github_rackspace_module_use_ssh:Rackspace module references should use SSH source paths:{}'.format(file)) in caplog.text
+            assert ('[PASS] github_rackspace_module_use_ssh:Rackspace module references should use SSH source paths:{}'.format(file)) in caplog.text
 
     def test_warns_if_module_has_http_ref(self, caplog):
         file = 'tests/test_module/module_github_hasref.tf'
