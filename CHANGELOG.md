a397fcb790f68084da2ea36126818d434c7b907b
John Titus <john.titus@rackspace.com>
2020-01-29 18:11:34 -0600

    Version 0.1.2 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+0.1.2 (2020-01-29)
+------------------
+
+* Enhancement: Include filename causing parsing error (#39)
+
 0.1.1 (2020-01-06)
 ------------------
 
--- tuvok/__version__.py
+++ tuvok/__version__.py
@@ -1,5 +1,5 @@
 __title__ = 'tuvok'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright Rackspace US, Inc. 2018'
 
