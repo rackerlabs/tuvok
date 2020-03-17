617c25e4afe254e5482d800ca60518444a133771
Chris White <chris.white@rackspace.com>
2020-03-17 10:04:09 -0500

    [MPSUPENG-852] Check for AWS provider block with no region
    * Added rule to tvok JSON file
    * Add tests and test sample TF files
    * Verified tests passed accordingly

---
+++ tests/test_provider.py
@@ -0,0 +1,22 @@
+from tuvok import cli
+from helpers.wrap import Wrap
+
+
+class TestProvider(object):
+    def setup(self):
+        self.main = cli.main
+
+    def teardown(self):
+        self.main = None
+
+    def test_fails_if_region_not_in_provider_block(self, caplog):
+        file = 'tests/test_provider/bad/main.tf'
+
+        with Wrap(self, [file], expect_exit=False):
+            assert ('[FAIL] provider_has_region:Provider block should have region declared:aws:{}'.format(file)) in caplog.text
+
+    def test_passes_if_region_in_provider_block(self, caplog):
+        file = 'tests/test_provider/good/main.tf'
+
+        with Wrap(self, [file], expect_exit=False):
+            assert ('[PASS] provider_has_region:Provider block should have region declared:{}'.format(file)) in caplog.text
