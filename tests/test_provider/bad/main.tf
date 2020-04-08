a65295317d58c1dfb446430e00ef6c7bab906df3
Chris White <chris.white@rackspace.com>
2020-04-08 11:08:49 -0500

    Fix issue with region provider check
    * The region check was not restricted to aws providers causing
      providers such as random to trigger errors
    * Break out provider checks into separate files to not cause
      collisions with multiple provider checks

--- tests/test_provider/bad/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  version = "~> 2.1"
-}
-
-provider "aws" {
-  alias = "no_version"
-}
--- tests/test_provider/good/main.tf
+++ tests/test_provider/good/main.tf
@@ -2,3 +2,7 @@ provider "aws" {
   region  = "us-west-2"
   version = "~> 2.1"
 }
+
+provider "random" {
+  version = "~> 2.1"
+}
